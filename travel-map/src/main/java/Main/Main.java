63d15fb877f6fc1582b4f5751f1f21f1cf3496db
Miriam Mironiuc <miriam.mironiuc@student.upt.ro>
2020-05-07 21:13:44 +0300

    Merge in the same project - try1

---
+++ travel-map/src/main/java/Main/Main.java
@@ -0,0 +1,26 @@
+package Main;
+import java.awt.EventQueue;
+import java.sql.DriverManager;
+
+import Guest.GuestGui;
+import com.mysql.jdbc.Connection;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+
+                    String url = "jdbc:mysql://localhost:3306/travel-map";
+                    Connection con = (Connection) DriverManager.getConnection(url, "root", "TravelMap");
+                    GuestGui window = new GuestGui(con);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
