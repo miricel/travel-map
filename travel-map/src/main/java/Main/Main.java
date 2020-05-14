a3b1c7838e62b96caf2fc91f0f2ba87473bbf1f8
Miriam Mironiuc <miriam.mironiuc@student.upt.ro>
2020-05-14 16:20:52 +0300

    TravelerProfile

--- travel-map/src/main/java/Main/Main.java
+++ travel-map/src/main/java/Main/Main.java
@@ -1,26 +1,25 @@
-package Main;
+package ptMiri;
 import java.awt.EventQueue;
 import java.sql.DriverManager;
-
-import User.Guest.GuestGui;
 import com.mysql.jdbc.Connection;
 
 public class Main {
+	static String url;
+	static Connection con;
 
-
-    public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-
-                    String url = "jdbc:mysql://localhost:3306/travel-map";
-                    Connection con = (Connection) DriverManager.getConnection(url, "root", "TravelMap");
-                    GuestGui window = new GuestGui(con);
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					
+					url = "jdbc:mysql://localhost:3306/BookingApp";
+					con = (Connection) DriverManager.getConnection(url, "denisa", "denisa");
+					GuestGui window = new GuestGui(con);
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 }
--- travel-map/src/main/java/User/User.java
+++ travel-map/src/main/java/User/User.java
@@ -1,4 +1,6 @@
-package User;import java.awt.Image;
+package ptMiri;
+
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -7,84 +9,88 @@ import java.io.InputStream;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
-
 import com.mysql.jdbc.Blob;
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.PreparedStatement;
 
-public abstract class User {
+abstract class User {
 
-    protected int id;
-    protected Connection con;
-
-    public User(java.sql.Connection con, int id) {
-        this.con = (Connection) con;
-        this.id = id;
-    }
-
-    public int getID() {
-        return this.id;
-    }
-
-    public String getStringColumn(String tableType, String column) throws SQLException {
-        Statement mystate = con.createStatement();
-        String query = "SELECT "+column+" FROM "+tableType;
-        query = query + " WHERE id="+id;
-        ResultSet myRS = mystate.executeQuery(query);
-        if(myRS.next())
-            return myRS.getString(1);
-        else return null;
-
-    }
-
-    public int getIntColumn(String tableType, String column) throws SQLException {
-        Statement mystate = con.createStatement();
-        String query = "SELECT "+column+" FROM "+tableType;
-        query = query + " WHERE  id="+id;
-        ResultSet myRS = mystate.executeQuery(query);
-        if(myRS.next())
-            return myRS.getInt(1);
-        else return -1;
-    }
-
-    public ImageIcon getProfilePicture(String tableType) throws SQLException, IOException {
-        Statement mystate = con.createStatement();
-        String query = "SELECT profilePic FROM "+tableType;
-        query = query + " WHERE  id="+id;
-        ResultSet myRS = mystate.executeQuery(query);
-        if(myRS.next()) {
-            Blob blob = (Blob) myRS.getBlob("profilePic");
-            InputStream is = blob.getBinaryStream();
-            BufferedImage bi = ImageIO.read(is);
-            Image image = bi;
-            ImageIcon ic = new ImageIcon(image);
-            return ic;
-        }
-        return null;
-    }
-
-    public void uploadPic(String tableType, String path) throws FileNotFoundException, SQLException {
-        InputStream in = new FileInputStream(path);
-        String query="UPDATE " + tableType + " SET profilePic = ? "+" WHERE id=" + id;
-        PreparedStatement mystate = (PreparedStatement) con.prepareStatement(query);
-        mystate.setBlob(1, in);
-        mystate.execute();
-        mystate.close();
-    }
-
-    public void setStringColumn(String tableType, String column, String newData) throws SQLException {
-        String query="UPDATE " + tableType + " SET "+ column + " = ? "+" WHERE id=" + id;
-        PreparedStatement mystate = (PreparedStatement) con.prepareStatement(query);
-        mystate.setString(1, newData);
-        mystate.execute();
-        mystate.close();
-    }
-
-    public void logOut() throws SQLException {
-        con.close();
-    }
-
+	protected int id;
+	protected Connection con;
+	
+	public User(java.sql.Connection con, int id) {
+		this.con = (Connection) con;
+		this.id = id;
+	}
+	
+	public int getID() {
+		return this.id;
+	}
+	
+	public int getIdUser(String tableType, String username) throws SQLException {
+		Statement mystate = con.createStatement();
+		String query = "SELECT id FROM "+tableType;
+		query = query + " WHERE username='"+username+"'";
+		ResultSet myRS = mystate.executeQuery(query);		
+		if(myRS.next())
+			return myRS.getInt(1);
+		else throw new SQLException();
+	}
+	
+	public String getStringColumn(String tableType, String column) throws SQLException {	
+		Statement mystate = con.createStatement();
+		String query = "SELECT "+column+" FROM "+tableType;
+		query = query + " WHERE id="+id;
+		ResultSet myRS = mystate.executeQuery(query);
+		if(myRS.next())
+			return myRS.getString(1);
+		else throw new SQLException();
+		
+	}
+	
+	public int getIntColumn(String tableType, String column) throws SQLException {
+		Statement mystate = con.createStatement();
+		String query = "SELECT "+column+" FROM "+tableType;
+		query = query + " WHERE  id="+id;
+		ResultSet myRS = mystate.executeQuery(query);		
+		if(myRS.next())
+			return myRS.getInt(1);
+		else throw new SQLException();
+	}
+	
+	public ImageIcon getProfilePicture(String tableType) throws SQLException, IOException {
+		Statement mystate = con.createStatement();
+		String query = "SELECT profilePic FROM "+tableType;
+		query = query + " WHERE  id="+id;
+		ResultSet myRS = mystate.executeQuery(query);		
+		if(myRS.next()) {
+			Blob blob = (Blob) myRS.getBlob("profilePic");
+			InputStream is = blob.getBinaryStream();
+			BufferedImage bi = ImageIO.read(is);
+			Image image = bi;
+			ImageIcon ic = new ImageIcon(image);
+			return ic;
+			}
+		throw new SQLException();
+	}
+	
+	public void uploadPic(String tableType, String path) throws FileNotFoundException, SQLException {
+		InputStream in = new FileInputStream(path);
+		String query="UPDATE " + tableType + " SET profilePic = ? "+" WHERE id=" + id;
+		PreparedStatement mystate = (PreparedStatement) con.prepareStatement(query);
+		mystate.setBlob(1, in);
+		mystate.execute();
+		mystate.close();
+	}
+	
+	public void setStringColumn(String tableType, String column, String newData) throws SQLException {
+		String query="UPDATE " + tableType + " SET "+ column + " = ? "+" WHERE id=" + id;
+		PreparedStatement mystate = (PreparedStatement) con.prepareStatement(query);
+		mystate.setString(1, newData);
+		mystate.execute();
+		mystate.close();
+	}
+	
 }
